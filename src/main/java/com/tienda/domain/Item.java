1695a30b649c1adc21051a38426ddaf001f3bcb4
joanjoel2003 <134662582+joanjoel2003@users.noreply.github.com>
2024-08-11 20:57:50 -0600

    Semana 11 y 12 de Desarrollo de patrones Web

---
+++ src/main/java/com/tienda/domain/Item.java
@@ -0,0 +1,32 @@
+package com.tienda.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+
+public class Item extends Producto {
+
+    private int cantidad;
+    
+    public Item(){
+    
+    }
+    
+    public Item(Producto producto){
+        super.setIdProducto(producto.getIdProducto());
+        super.setCategoria(producto.getCategoria());
+        super.setDetalle(producto.getDetalle());
+        super.setDescripcion(producto.getDescripcion());
+        super.setPrecio(producto.getPrecio());
+        super.setExistencias(producto.getExistencias());
+        super.setActivo(producto.isActivo());
+        super.setRutaImagen(producto.getRutaImagen());
+        this.cantidad = 0;
+    }
+    
+    
+
+    
+}
---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,20 @@
+package com.tienda.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-2c24.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-2c24-firebase-adminsdk-wgcj7-07b6fd1845.json";
+}
